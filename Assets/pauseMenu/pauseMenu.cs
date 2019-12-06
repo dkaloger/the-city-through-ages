03b92a43260bcdfe211a58171398e11ae0693aef
JoJoRogild <johanrogild@gmail.com>
2019-12-06 21:24:55 +0100

    puase menu added
    i greatet a pause menu that can mute an un mute and goto law screen and quit the game

---
+++ Assets/pauseMenu/pauseMenu.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class pauseMenu : MonoBehaviour
+{
+
+    public static bool GameIsPaused = false;
+
+    public GameObject PauseMenu;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                resume();
+            }
+            else
+            {
+                pause();
+            }
+        }
+    }
+    void resume()
+    {
+        PauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+    public void pause()
+    {
+        PauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+    //buttons
+    public void Quit()
+    {
+        Application.Quit();
+        Debug.Log("Quit");
+    }
+    public void Resume()
+    {
+        PauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+    public void LawScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void Mute()
+    {
+        AudioListener.pause = !AudioListener.pause;
+    }
+}
