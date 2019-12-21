0fa0bbf7e88c72d9a32fb3904ad2aa3bfa720bbb
JoJoRogild <johanrogild@gmail.com>
2019-12-21 16:55:19 +0100

    save system done just saving the pos of the cam

---
+++ Assets/scripts/savingSystem/saving.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+using UnityEngine;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public static class saving
+{
+    public static void savePosOfCam(testSaving player)
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
+        //string path = Application.persistentDataPath + "/player.save";
+        string path = Application.persistentDataPath + "/player.save";
+        FileStream stream = new FileStream(path, FileMode.Create);
+
+        posSave data = new posSave(player);
+
+        formatter.Serialize(stream, data);
+        stream.Close();
+    }
+
+    public static posSave loadPos()
+    {
+        string path = Application.persistentDataPath + "/player.save";
+
+        if (File.Exists(path))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream stream = new FileStream(path, FileMode.Open);
+
+            posSave data = formatter.Deserialize(stream) as posSave;
+            stream.Close();
+
+            return data;
+        }
+        else
+        {
+            Debug.LogError("Save File Not Found in " + path);
+            return null;
+        }
+    }
+}
---
+++ Assets/scripts/savingSystem/testSaving.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testSaving : MonoBehaviour
+{
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            saving.savePosOfCam(this);
+        }
+        else if (Input.GetKeyDown(KeyCode.K))
+        {
+            posSave data = saving.loadPos();
+
+            Vector3 position;
+            position.x = data.posOfCam[0];
+            position.y = data.posOfCam[1];
+            position.z = data.posOfCam[2];
+
+            transform.position = position;
+        }
+    }
+}
