0c332bf8304c1721b29a39d6db9fc137c2f71b1f
dkaloger <52400814+dkaloger@users.noreply.github.com>
2019-12-30 22:18:26 +0200

    Revert "Revert "Revert "Merge branch 'master' of https://github.com/dkaloger/the-city-through-ages"""
    This reverts commit 9dfd6a7949485b5adbab1d802d5d1f12305db431.

--- Assets/buildingbasescript.cs
+++ Assets/buildingbasescript.cs
@@ -5,22 +5,14 @@ using UnityEngine;
 public class buildingbasescript : MonoBehaviour
 {
     // Start is called before the first frame update
-    [SerializeField]
-    private Transform parent;
-    [SerializeField]
-        private bool doneonce = false;
+    void Start()
+    {
+        
+    }
 
     // Update is called once per frame
-
-
     void Update()
     {
-        transform.SetParent(parent, false);
-if(doneonce == false ){
-           
-            transform.position = new Vector3(0, 0, 1);
-            doneonce = true;
-        }
-       
+        transform.position = new Vector3(0, 0, 1);
     }
 }
