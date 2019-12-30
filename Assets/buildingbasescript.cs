1268255e9481660198bfbcce9ae2b62f2e2c9110
dkaloger <52400814+dkaloger@users.noreply.github.com>
2019-12-30 21:41:06 +0200

    misc

--- Assets/buildingbasescript.cs
+++ Assets/buildingbasescript.cs
@@ -6,16 +6,21 @@ public class buildingbasescript : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField]
-    Transform parent;
-    bool 
+    private Transform parent;
+    [SerializeField]
+        private bool doneonce = false;
 
     // Update is called once per frame
- 
-   
+
+
     void Update()
     {
-
         transform.SetParent(parent, false);
-        transform.position = new Vector3(0, 0, 1);
+if(doneonce == false ){
+           
+            transform.position = new Vector3(0, 0, 1);
+            doneonce = true;
+        }
+       
     }
 }
