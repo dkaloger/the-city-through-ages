eb5bc1b0ba44674f1d90199b204c61a11ec7440f
dkaloger <52400814+dkaloger@users.noreply.github.com>
2019-12-30 21:09:29 +0200

    -

--- Assets/buildingbasescript.cs
+++ Assets/buildingbasescript.cs
@@ -5,14 +5,17 @@ using UnityEngine;
 public class buildingbasescript : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    Transform parent;
+    bool 
 
     // Update is called once per frame
+ 
+   
     void Update()
     {
+
+        transform.SetParent(parent, false);
         transform.position = new Vector3(0, 0, 1);
     }
 }
