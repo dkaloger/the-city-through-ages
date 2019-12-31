c814c977698c5cbebfb4f114d60b09c0763e9ea6
JoJoRogild <johanrogild@gmail.com>
2019-12-31 12:13:37 +0100

    fixed how to get the decimal number

--- Assets/wheels/buildWheel/placebuilding.cs
+++ Assets/wheels/buildWheel/placebuilding.cs
@@ -56,79 +56,91 @@ public class placebuilding : MonoBehaviour
     void Update()
     {
         testBuilding = objbuildings[buildWheelOptions1.curTier * 8 + glowmaster1.selectedring];
-        Debug.Log(buildWheelOptions1.curTier * 8 + glowmaster1.selectedring);
-        if (testInt < 200)
+        //Debug.Log(buildWheelOptions1.curTier * 8 + glowmaster1.selectedring);
+
+        point = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        double carPosX = tileMapPos.transform.position.x;
+        double carPosX2 = (int)carPosX;
+
+        //float p3 = p - p2;
+        double carPosX3 = carPosX - 107f;
+
+         //Single f = tileMapPos.transform.position.x - (int)tileMapPos.transform.position.x;
+
+        float f = tileMapPos.position.x;
+
+        float f2 = f - (int)f;
+
+        Debug.Log(f2);
+
+        /*
+        Debug.Log(Double.Parse(f.ToString()) - (int)f);
+
+        double d2 = Double.Parse(f.ToString());
+
+        double d3 = d2 - (int)d2;
+
+
+
+        Debug.Log(d3);
+        */
+
+        //Debug.Log(buildingPosXTest[0]);
+        //Debug.Log("isInList2");
+
+        //float test = 51f;
+        //Debug.Log(test % 2);
+        //float double_value = -30.555F;
+        //Debug.Log((int)((double_value - (int)double_value) * 100) + "this is a test");
+        //float decimalPart = 22.45F;
+        //decimalPart.ToString("F0" + "this should show 22");-
+        //Debug.Log(Mathf.Round(decimalPart));
+        //point.x = (int)Camera.main.ScreenToWorldPoint(Input.mousePosition.x);
+        point = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //point = (int)Input.mousePosition.y;
+
+        if (Input.GetMouseButtonDown(0) && orderWheelOnP == false)
         {
-            point = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            buildingPosXTest.Add(testInt * 1.5f + 0.25f);
-            testInt++;
-
-            double carPosX = tileMapPos.transform.position.x;
-            double carPosX2 = (int)carPosX;
-
-            //float p3 = p - p2;
-            double carPosX3 = carPosX - 107f;
-
-            Single f = tileMapPos.transform.position.x - (int)tileMapPos.transform.position.x;
-
-            double d2 = Double.Parse(f.ToString());
-
-            Debug.Log(buildingPosXTest[0]);
-
-            Debug.Log("isInList2");
-
-            //float test = 51f;
-            //Debug.Log(test % 2);
-            //float double_value = -30.555F;
-            //Debug.Log((int)((double_value - (int)double_value) * 100) + "this is a test");
-            //float decimalPart = 22.45F;
-            //decimalPart.ToString("F0" + "this should show 22");-
-            //Debug.Log(Mathf.Round(decimalPart));
-            //point.x = (int)Camera.main.ScreenToWorldPoint(Input.mousePosition.x);
-            point = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            //point = (int)Input.mousePosition.y;
-
-            if (Input.GetMouseButtonDown(0) && orderWheelOnP == false)
-            {
-                //if ((int)point.y % 1 == 0.5F && (int)point.x % 1 == 0.25F || (int)point.x % 1 == 0.75F)
-                var myBuilding = Instantiate(testBuilding, new Vector3(point.x, point.y, 1), Quaternion.identity);
-                //myBuilding.transform.parent = gridObj.transform;
-                float myCheckPosY = myBuilding.transform.position.y;
-                float myCheckPosY2 = Mathf.Floor(myCheckPosY);
-                myCheckPosY = myCheckPosY - myCheckPosY2;
-                float myCheckPosX = myBuilding.transform.position.x;
-                float myCheckPosX2 = Mathf.Floor(myCheckPosX);
-                myCheckPosX = myCheckPosX - myCheckPosX2;
-                float xUnEven = myCheckPosX % 2;
-                float comTest1 = 22.55f;
-                float comTest2 = 22.66f;
-                float test2 = 1.5f;
-                float test = 0;
-                Debug.Log(comTest2.CompareTo(comTest1) + " com thing");//this returns -1 or 1,  1 if is the first is greater than the other the other is the oppersit
+            //if ((int)point.y % 1 == 0.5F && (int)point.x % 1 == 0.25F || (int)point.x % 1 == 0.75F)
+            var myBuilding = Instantiate(testBuilding, new Vector3(point.x, point.y, 1), Quaternion.identity);
+            //myBuilding.transform.parent = gridObj.transform;
+            float myCheckPosY = myBuilding.transform.position.y;
+            float myCheckPosY2 = Mathf.Floor(myCheckPosY);
+            myCheckPosY = myCheckPosY - myCheckPosY2;
+            float myCheckPosX = myBuilding.transform.position.x;
+            float myCheckPosX2 = Mathf.Floor(myCheckPosX);
+            myCheckPosX = myCheckPosX - myCheckPosX2;
+            float xUnEven = myCheckPosX % 2;
+            float comTest1 = 22.55f;
+            float comTest2 = 22.66f;
+            float test2 = 1.5f;
+            float test = 0;
+            Debug.Log(comTest2.CompareTo(comTest1) + " com thing");//this returns -1 or 1,  1 if is the first is greater than the other the other is the oppersit
                                                                        //Debug.Log(myCheckPosX);
 
-                techTree.GetComponent<techTree>().addBuilding(GetComponent<glowmaster>().selectedring - 1);
-                float nowGrid = 1f / gridSizeX;
-                float nowGridY = 1f / gridSizeY;
-                if (myCheckPosY >= 0.5)
-                {
+            techTree.GetComponent<techTree>().addBuilding(GetComponent<glowmaster>().selectedring - 1);
+            float nowGrid = 1f / gridSizeX;
+            float nowGridY = 1f / gridSizeY;
+            if (myCheckPosY >= 0.5)
+            {
 
                     //Debug.Log("you made it to y1");
-                    if (myCheckPosX >= 0.5)
+                if (myCheckPosX >= 0.5)
+                {
+                    Vector3 MyBuildPos;
+                    MyBuildPos.x = Mathf.Round(myBuilding.transform.position.x * nowGrid) / nowGrid;
+                    MyBuildPos.y = Mathf.Round(myBuilding.transform.position.y * nowGridY) / nowGridY;
+                    MyBuildPos.z = 1;
+                    myBuilding.transform.position = MyBuildPos;
+                    myCheckPosX2 = Mathf.Floor(myCheckPosX);
+                    myCheckPosX = myCheckPosX - myCheckPosX2;
+                    if (myCheckPosX == 0.5)
                     {
-                        Vector3 MyBuildPos;
-                        MyBuildPos.x = Mathf.Round(myBuilding.transform.position.x * nowGrid) / nowGrid;
-                        MyBuildPos.y = Mathf.Round(myBuilding.transform.position.y * nowGridY) / nowGridY;
-                        MyBuildPos.z = 1;
-                        myBuilding.transform.position = MyBuildPos;
-                        myCheckPosX2 = Mathf.Floor(myCheckPosX);
-                        myCheckPosX = myCheckPosX - myCheckPosX2;
-                        if (myCheckPosX == 0.5)
-                        {
-                            myBuilding.SetActive(false);
-                        }
-                        else if (myCheckPosX == 0)
-                        {
+                        myBuilding.SetActive(false);
+                    }
+                    else if (myCheckPosX == 0)
+                     {
                             myBuilding.SetActive(false);
                         }
                     }
@@ -216,9 +228,8 @@ public class placebuilding : MonoBehaviour
         //Debug.Log(decimalPart.ToString("F0" + "this should show 22"));
         //techTree.GetComponent<techTree>().addBuilding(GetComponent<glowmaster>().selectedring - 1);
 
-        void tierChange(int tier)
-        {
-            tiers = tier * 8;
-        }
+    public void tierChange(int tier)
+    {
+        tiers = tier * 8;
     }
 }
