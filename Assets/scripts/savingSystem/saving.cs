6fde9124637ef619c09b6a724cbabc3a9d0f4edf
JoJoRogild <johanrogild@gmail.com>
2020-01-14 21:08:36 +0100

    saving system nearly done
    i just need place building system to place buildings first

--- Assets/scripts/savingSystem/saving.cs
+++ Assets/scripts/savingSystem/saving.cs
@@ -17,7 +17,7 @@ public static class saving
         stream.Close();
     }
 
-    public static posSave loadPos()
+    public static posSave loadPosOfCam()
     {
         string path = Application.persistentDataPath + "/player.save";
 
@@ -37,4 +37,38 @@ public static class saving
             return null;
         }
     }
+    //real in game save the others was just an test
+    public static savingBuildPos loadPosOfBuilding()
+    {
+        string path = Application.persistentDataPath + "/buildingPos.save";
+
+        if (File.Exists(path))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream stream = new FileStream(path, FileMode.Open);
+
+            savingBuildPos data = formatter.Deserialize(stream) as savingBuildPos;
+            stream.Close();
+
+            return data;
+        }
+        else
+        {
+            Debug.LogError("Save File Not Found in " + path);
+            return null;
+        }
+    }
+    public static void savePosOfBuilding (testSaving Cam)
+    {
+        //greates the encrypter
+        BinaryFormatter formatter = new BinaryFormatter();
+        string path = Application.persistentDataPath + "/buildingPos.save";
+        FileStream stream = new FileStream(path, FileMode.Create);
+        //send the dater further in the system
+        savingBuildPos data = new savingBuildPos(Cam);
+        //encrypt the data
+        formatter.Serialize(stream, data);
+        stream.Close();
+    }
+
 }
--- Assets/scripts/savingSystem/testSaving.cs
+++ Assets/scripts/savingSystem/testSaving.cs
@@ -5,15 +5,44 @@ using UnityEngine;
 public class testSaving : MonoBehaviour
 {
 
+    public List<float> buildingsPosX = new List<float>();
+    public List<float> buildingsPosY = new List<float>();
+    public List<string> buildingsName = new List<string>();
+
+    void Start()
+    {
+        
+    }
+
     void Update()
     {
+
+        //in game content
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            savingBuildPos buildingData = saving.loadPosOfBuilding();
+
+            //put the text file into the game
+            for (int i = 0; i < buildingData.posOfBuildingX.Length; i++)
+            {
+                buildingsPosX[i] = buildingData.posOfBuildingX[i];
+                buildingsPosY[i] = buildingData.posOfBuildingY[i];
+                buildingsName[i] = buildingData.buildingsNames[i];
+            }
+        }
+        //gives the system the data
+        else if (Input.GetKeyDown(KeyCode.P))
+        {
+            saving.savePosOfBuilding(this);
+        }
+        //test content just to see if ot worked
         if (Input.GetKeyDown(KeyCode.V))
         {
             saving.savePosOfCam(this);
         }
         else if (Input.GetKeyDown(KeyCode.K))
         {
-            posSave data = saving.loadPos();
+            posSave data = saving.loadPosOfCam();
 
             Vector3 position;
             position.x = data.posOfCam[0];
