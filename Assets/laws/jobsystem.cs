19d1814f49f0f645945b501b2d7b654c929d5197
dkaloger <52400814+dkaloger@users.noreply.github.com>
2020-01-29 22:01:33 +0200

    job system part one done

--- Assets/laws/jobsystem.cs
+++ Assets/laws/jobsystem.cs
@@ -13,25 +13,22 @@ public class jobsystem : MonoBehaviour
    
    
     public Slider choptrees;
-
+    //for a weird   reason they are inverted it doesnt cause problems but be careful
     public Slider mines;
-    public int minei;
     public int choptreei;
+    public int minei;
     
     void Update() {
     minei = Mathf.RoundToInt(mines.value);
-        wedg1.fillAmount = choptrees.value;
-        wedg2.fillAmount = mines.value;
+        choptreei = Mathf.RoundToInt(choptrees.value);
         PlayerPrefs.SetInt("mine",minei);
            PlayerPrefs.SetInt("chop",choptreei);
-           
+        wedg1.fillAmount = choptrees.value / 100f;
+        wedg2.fillAmount = mines.value / 100f;
+        choptrees.value = 100 - mines.value;
+        mines.value = 100 - choptrees.value;
+       
     }
 
-     public void s1changed(){
-        mines.value = 100 - choptrees.value;
-    }
-    public void s2changed()
-    {
-        choptrees.value = 100 - mines.value;
-    }
+   
 }
