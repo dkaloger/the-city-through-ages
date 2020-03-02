d7a6033dccba15961bedaf1f78e23e5f2765f87f
dkaloger <52400814+dkaloger@users.noreply.github.com>
2020-03-02 22:35:51 +0200

    secret icons hover to reveal

--- Assets/resources/displays/fooddipslay.cs
+++ Assets/resources/displays/fooddipslay.cs
@@ -5,13 +5,18 @@ using TMPro;
 public class fooddipslay : MonoBehaviour
 {
     public int food;
+    public int unfood;
     private TextMeshProUGUI textMesH;
 
+    public TextMeshProUGUI textMesH1;
+   
+    public GameObject sfood;
     // Start is called before the first frame update
     void Start()
     {
         textMesH = GetComponent<TextMeshProUGUI>();
         food = 1000;
+        unfood = 1000;
         PlayerPrefs.SetInt("food", food);
     }
 
@@ -19,7 +24,15 @@ public class fooddipslay : MonoBehaviour
     void Update()
     {
         textMesH.text = food.ToString();
-     
+        textMesH1.text = unfood.ToString();
         PlayerPrefs.SetInt("food",food);
+    }
+    void OnMouseOver()
+    {
+        sfood.SetActive(true);
+    }
+    void OnMouseExit()
+    {
+        sfood.SetActive(false);
     }
 }
--- Assets/resources/displays/popdisp.cs
+++ Assets/resources/displays/popdisp.cs
@@ -5,15 +5,17 @@ using TMPro;
 public class popdisp : MonoBehaviour
 {
     public int pop;
+    int maxpop = 10;
     private TextMeshProUGUI textMesH;
-
+    public TextMeshProUGUI textMesH1;
+    public GameObject spop;
     // Start is called before the first frame update
     void Start()
     {
         textMesH = GetComponent<TextMeshProUGUI>();
         pop = 2;
-       
-       
+        textMesH1.text = maxpop.ToString();
+
     }
 
     // Update is called once per frame
@@ -21,5 +23,13 @@ public class popdisp : MonoBehaviour
     {
         textMesH.text = pop.ToString();
         PlayerPrefs.SetInt("pop", pop);
+    }
+    void OnMouseOver()
+    {
+        spop.SetActive(true);
+    }
+    void OnMouseExit()
+    {
+        spop.SetActive(false);
     }
 }
