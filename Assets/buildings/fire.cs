5789d590260a2ab8e7fc28731a21115dab083d74
dkaloger <52400814+dkaloger@users.noreply.github.com>
2020-03-08 10:50:43 +0200

    fire

---
+++ Assets/buildings/fire.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fire : MonoBehaviour
+{
+ public   fooddipslay fd;
+  public  int t ;
+ public   int fooddelay;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     if  (fd.unfood > 0)
+        {
+
+            t++;
+            if(t > fooddelay)
+            {
+                fd.food += 2;
+                fd.unfood -= 1;
+                t = 0;
+            }
+        }  
+    }
+}
